f408927095b26082eb8b187badcaf0c81248ab83
Diego <diego@fyneworks.com>
2020-03-20 15:10:10 +0000

    added styles in scss

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux';
 import { store } from './_helpers';
 import { App } from './App';
 
+import './styles'
+
 // setup fake backend
 import { configureFakeBackend } from './_helpers';
 configureFakeBackend();
--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,29 @@ module.exports = {
                     presets: ['@babel/preset-react']
                 }
 
-            }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader!css-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
         ]
     },
     plugins: [new HtmlWebpackPlugin({
